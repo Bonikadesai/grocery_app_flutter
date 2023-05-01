aa930225e8c3856bf6ec03cb36524271e5354bea
bonikadeasi <7951bonikadesai@gmail.com>
2023-05-01 10:24:02 +0530

    Grossery App

---
+++ lib/utils/model.dart
@@ -0,0 +1,15 @@
+class product {
+  String? name;
+  String? image;
+  String? price;
+  String? detail;
+  String? quanty;
+
+  product({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.detail,
+    required this.quanty,
+  });
+}
